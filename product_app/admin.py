31bf04efa3b57be85cce5610ebdd485cc5ba6e9b
hoittoken <aubakirovma@gmail.com>
2025-01-29 09:29:30 +0200

    Изменения

--- product_app/admin.py
+++ product_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Product, Project
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    search_fields = ('name',)
+    list_filter = ('created_at',)
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'product', 'start_date', 'end_date')
+    search_fields = ('name',)
+    list_filter = ('start_date', 'end_date', 'product')
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Project, ProjectAdmin)
